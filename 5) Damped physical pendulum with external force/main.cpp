0dab24a74b0bebc94612bb293723ba162f250244
pkkvn <71311239+pkkvn@users.noreply.github.com>
2021-12-18 00:17:27 +0300

    Add files via upload

---
+++ 5) Damped physical pendulum with external force/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+
+
+
+template <typename T>
+void data_t(float dt, int N, std::vector<T> &x, std::vector<T> &v, std::vector<T> &t, double gamma, double w0, double w, double F){
+
+    T x_n = 0;
+    T v_n = 0;
+    T t_n = 0;
+
+    x[0] = x_n;
+    v[0] = v_n;
+    t[0] = t_n;
+
+    for (int i = 1; i < N; i++){
+
+        x_n = x[i-1] + v[i-1] * dt;
+        v_n = v[i-1] + (F * std::cos(w * t[i-1]) - (2 * gamma * v[i-1] + w0 * w0 * std::sin(x[i-1]))) * dt;
+        t_n = t[i-1] + dt;
+
+        x[i] = x_n;
+        v[i] = v_n;
+        t[i] = t_n;
+    }
+
+}
+
+
+template <typename T>
+void predict_correct_data_t(float dt, int N, std::vector<T> &x, std::vector<T> &v, std::vector<T> &t, double gamma, double w0, double w, double F){
+
+    T x_n = 0;
+    T v_n = 0;
+    T t_n = 0;
+
+    x[0] = x_n;
+    v[0] = v_n;
+    t[0] = t_n;
+
+    for (int i = 1; i < N; i++){
+        T x_pred = x[i-1] + v[i-1] * dt;
+        T v_pred = v[i-1] + (F * std::cos(w * t[i-1]) - (2 * gamma * v[i-1] + w0 * w0 * std::sin(x[i-1]))) * dt;
+
+        x_n = x[i-1] + dt * (v[i-1] + v_pred) / 2;
+        v_n = v[i-1] + dt * (F * std::cos(w * t[i-1]) + F * std::cos(w * (t[i-1] + dt)) - (2 * gamma * v[i-1] + w0 * w0 * std::sin(x[i-1]) + 2 * gamma * v_pred + w0 * w0 * std::sin(x_pred))) / 2;
+        t_n = t[i-1] + dt;
+
+        x[i] = x_n;
+        v[i] = v_n;
+        t[i] = t_n;
+    }
+
+}
+
+
+int main(int atgc, char *argv[]){
+
+    std::string path1 = argv[1];
+    std::string path2 = argv[2];
+
+    long double dt = atof(argv[3]);
+    double gamma = atof(argv[4]);
+    double w0 = atof(argv[5]);
+    double w = atof(argv[6]);
+
+    int T = 1000;
+    int N = int(T/dt);
+
+    std::vector<double> x = std::vector<double>(N);
+	std::vector<double> v = std::vector<double>(N);
+	std::vector<double> t = std::vector<double>(N);
+
+
+	double F = 10;
+
+	data_t(dt, N, t, x, v, gamma, w0, w, F);
+
+        remove("numbers1.txt");
+        std::ofstream out1(path1);
+        out1.open("numbers1.txt", std::ios::app);
+
+
+        if (out1.is_open())
+        {
+            for (int i = 0; i < N; i++){
+                out1 << x[i] << " " << v[i] << " " << t[i] << "\n";
+            }
+        }
+
+	x.resize(0);
+	v.resize(0);
+	t.resize(0);
+
+        predict_correct_data_t(dt, N, t, x, v, gamma, w0, w, F);
+
+        remove("numbers2.txt");
+        std::ofstream out2(path2);
+        out2.open("numbers2.txt", std::ios::app);
+
+        if (out2.is_open())
+        {
+            for (int i = 0; i < N; i++){
+                out2 << x[i] << " " << v[i] << " " << t[i] << "\n";
+            }
+        }
+
+
+}
+
+
